1891ac3f2d4be2f2f1a628338c81b9a7f0b8cea0
johnreienriquez <johnrei.enriquez@gmail.com>
2020-10-11 01:10:15 +0800

    update: notes for xcape

--- ubuntu/key-config.sh
+++ ubuntu/key-config.sh
@@ -4,6 +4,9 @@
 export DOTFILES_DIR=~/Code/dotfiles
 # make CapsLock behave like Ctrl:
 setxkbmap -option ctrl:nocaps
+# Make sure xcap is installed for below to work
+# sudo apt-get install xcape
+
 # make short-pressed Ctrl behave like Escape:
 xcape -e 'Control_L=Escape'
 
