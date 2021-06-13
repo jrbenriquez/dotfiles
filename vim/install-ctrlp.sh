117bcf48a873425c702e781306db48d3571cc367
johnreienriquez <johnrei.enriquez@gmail.com>
2021-06-13 22:58:21 +0800

    update: script plus i3 config

---
+++ vim/install-ctrlp.sh
@@ -0,0 +1,5 @@
+cd ~/.vim
+git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
+echo "Add this to your runtime if not already added"
+echo "set runtimepath^=~/.vim/bundle/ctrlp.vim"
+
