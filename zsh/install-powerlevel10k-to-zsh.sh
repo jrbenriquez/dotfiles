fabfda72794947b9ec9cdbf7ba7fc7f19f55a963
johnreienriquez <johnrei.enriquez@gmail.com>
2025-01-15 17:34:04 +0800

    cleanup

--- zsh/install-powerlevel10k-to-zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>! ~/.zshrc
-echo 'source ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
